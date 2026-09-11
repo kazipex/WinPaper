20dce331620d02148593c4f178a425b06dfa3940
kazipex <lexmaffei@outlook.com>
2026-09-11 22:09:02 +0100

    refactor: remove index.html and update homepage URL in package.json; fix route path in App.tsx

--- winpaper/src/App.tsx
+++ winpaper/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div style={{ height: "75px" }} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/homepage" element={<Home />} />
       </Routes>
     </>
   );
