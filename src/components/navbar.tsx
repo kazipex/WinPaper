081cb8e120cc309f174f7d640b84e48f308efc75
kazipex <lexmaffei@outlook.com>
2026-09-11 22:57:51 +0100

    feat: update Navbar styling and add new icon assets

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,20 +14,20 @@ const Navbar = () => {
       >
         <Box
           sx={{
-            width: "50vw",
+            mx: "20px",
             height: "75px",
             backgroundColor: "white",
             borderRadius: "0px 0px 10px 10px",
             display: "flex",
             alignItems: "center",
-            justifyContent: "left",
             border: "1px solid #e5e5e5",
             boxShadow: "5px 5px 10px #e5e5e5",
             flexDirection: "row",
+            justifyContent: "space-between",
           }}
         >
-          <Box sx={{ mx: "20px"}}>
-            <Box sx={{ fontSize: "20px", color: "black" }}>WINPAPER</Box>
+          <Box sx={{ mx: "40px", maxWidth: "200px" }}>
+            <Box sx={{ fontSize: "20px", color: "black", }}>WINPAPER</Box>
           </Box>
         </Box>
       </Box>
